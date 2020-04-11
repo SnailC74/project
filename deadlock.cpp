41781b49061c287906bf8d7cef680cc7501ddbb4
SnailC74 <60499437+SnailC74@users.noreply.github.com>
2020-04-10 21:20:35 -0600

    Update deadlock.cpp

--- deadlock.cpp
+++ deadlock.cpp
@@ -3,9 +3,8 @@
 //
 
 public class StudentAccount {
-  private:
-  String name;
-  doublic balance;
+  private String name;
+  private doublic balance;
   
   public StudentAccount(string name) {
     this.name = name;
@@ -26,3 +25,36 @@ public class StudentAccount {
   public String toString() {
         return name;
   }
+}
+
+public class Transfer extends threads {
+  private String id;
+  private StudentAccount from;
+  private studentAccount to;
+  private double amount;
+  
+  public Transfer(String id, StudentAccount from, StudentAccount to, double amount) {
+    this.id = id;
+    this .from = from;
+    this.to = to;
+    this.amount = amount;
+  }
+  
+  public void move() {
+    synchronized (from) {
+      from.withdraw(amount);
+            try {
+                Thread.sleep(500);
+                } catch (InterruptedException e) { }
+                synchronized (to) {
+                  to.deposit(amount);
+                }
+    }
+    System.out.println(amount + "is transfered from " + from + " to " + to);
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    StudentAccount
+
