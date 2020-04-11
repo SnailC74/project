cc6acdbb1ee76458a256e4ae2d840a80821bfd24
SnailC74 <60499437+SnailC74@users.noreply.github.com>
2020-04-10 21:09:28 -0600

    Update deadlock.cpp

--- deadlock.cpp
+++ deadlock.cpp
@@ -1,8 +1,28 @@
-#include <iostream>
-#include <cstdlib>
-#include <cstdio>
-#include <pthread.h>
-#include <sys/types.h>
-#include <unistd.h>
+//
+//  This is going to explain that the deadlock using threads
+//
 
-
+public class StudentAccount {
+  private:
+  String name;
+  doublic balance;
+  
+  public StudentAccount(string name) {
+    this.name = name;
+  }
+  
+  public void withdraw(double balance) {
+    this.balance -= balance;
+  }
+  
+  public void deposit(double balance) {
+    this.balance += balance;
+  }
+  
+  public double getBalance() {
+    return this.balance;
+  }
+  
+  public String toString() {
+        return name;
+  }
